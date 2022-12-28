e6b375d9810d3974146813ac48daa27bdd8dab3a
Melny <marek.melnycok@gmail.com>
2022-12-28 22:38:33 +0100

    Update

--- js/index.js
+++ js/index.js
@@ -1,10 +1,2 @@
-function importData() {
-  let input = document.createElement("input");
-  input.type = "file";
-  input.onchange = (_) => {
-    // you can use this method to get file and perform respective operations
-    let files = Array.from(input.files);
-    console.log(files);
-  };
-  input.click();
-}
+console.log("Hello" + "World");
+console.log("Hello" + " " + "World");
